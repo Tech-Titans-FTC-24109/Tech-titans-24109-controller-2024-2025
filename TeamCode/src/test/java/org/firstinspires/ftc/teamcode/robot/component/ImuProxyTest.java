b32733fd717cf9e1bad019a74d768f6b0547c9fc
Ralph <rqode@users.noreply.github.com>
2025-11-17 08:51:00 -0500

    Add unsupported operation tests ImuProxy

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/robot/component/ImuProxyTest.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/robot/component/ImuProxyTest.java
@@ -2,15 +2,43 @@ package org.firstinspires.ftc.teamcode.robot.component;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ImuProxyTest {
 
-    @Test
-    @DisplayName("WHEN condition EXPECT outcome")
-    void WHEN_condition_EXPECT_outcome() {
-        assertThat("Not implemented", is("Implemented"));
+    @Nested
+    @DisplayName("Unsupported methods")
+    public class UnsupportedMethods {
+
+        @Test
+        @DisplayName("WHEN proxy EXPECT cannot call initialize")
+        void WHEN_proxy_EXPECT_cannotCallInitialize() {
+            IMU imu = new Imu.ImuProxy(TestIMU.createIMU());
+
+            assertThrows(UnsupportedOperationException.class, () ->
+                    imu.initialize(TestIMU.DEFAULT_PARAMETERS));
+        }
+
+        @Test
+        @DisplayName("WHEN proxy EXPECT cannot call resetDeviceConfigurationForOpMode")
+        void WHEN_proxy_EXPECT_cannotCallResetDeviceConfigurationForOpMode() {
+            IMU imu = new Imu.ImuProxy(TestIMU.createIMU());
+
+            assertThrows(UnsupportedOperationException.class, imu::resetDeviceConfigurationForOpMode);
+        }
+
+        @Test
+        @DisplayName("WHEN proxy EXPECT cannot call close")
+        void WHEN_proxy_EXPECT_cannotCallClose() {
+            IMU imu = new Imu.ImuProxy(TestIMU.createIMU());
+
+            assertThrows(UnsupportedOperationException.class, imu::close);
+        }
     }
 }
