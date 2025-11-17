29cac34284d6dd7c2b05ebe34ced1233f861f9ea
Ralph <rqode@users.noreply.github.com>
2025-11-16 23:26:51 -0500

    Experiment with Github actions

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/robot/component/ImuProxyTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.robot.component;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ImuProxyTest {
+
+    @Test
+    @DisplayName("WHEN condition EXPECT outcome")
+    void WHEN_condition_EXPECT_outcome() {
+        assertThat("Not implemented", is("Implemented"));
+    }
+}
