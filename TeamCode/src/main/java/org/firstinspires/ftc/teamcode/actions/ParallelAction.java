20d5e8f8d6c6208a33144fe74d7c8bcad22ac1d0
THEGUYFROMEBAY <akarasharingan@gmail.com>
2025-11-21 20:17:02 -0500

    Implemented more todos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/ParallelAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/ParallelAction.java
@@ -2,11 +2,18 @@ package org.firstinspires.ftc.teamcode.actions;
 
 // TODO - implement
 public class ParallelAction implements ICompositeAction {
+    private boolean isInitialized = false;
+    private boolean isFinished = false;
+    private boolean isStopped = false;
 
     private final IAction[] parallelActions;
 
     public ParallelAction(IAction... parallelActions) {
         this.parallelActions = parallelActions;
+        if (parallelActions.length == 0) {
+            isFinished = true;
+            isStopped = true;
+        };
     }
 
     @Override
@@ -16,37 +23,47 @@ public class ParallelAction implements ICompositeAction {
 
     @Override   // TODO implement
     public boolean init() {
+        if (isInitialized()) throw new IllegalStateException("Can not reinitialize after initialization");
         for (IAction action :
                 parallelActions) {
             action.init();
         }
-    }
-
-    @Override   // TODO implement
-    public boolean isInitialized() {
+        isInitialized = true;
         return true;
     }
 
     @Override   // TODO implement
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+
+    @Override   // TODO implement
     public boolean iterate() {
+        if (!isInitialized()) throw new IllegalStateException("Can not iterate before initialization");
+        if (parallelActions.length == 0) throw new IllegalStateException("Can not iterate with a empty list of actions");
         for (IAction action :
                 parallelActions) {
             action.iterate();
         }
+        return true;
     }
 
     @Override   // TODO implement
     public boolean isFinished() {
-        return false;
+        return isFinished;
     }
 
     @Override   // TODO implement
     public boolean stop() {
-        return false;
+        for (IAction action :
+                parallelActions) {
+            action.stop();
+        }
+        return true;
     }
 
     @Override   // TODO implement
     public boolean isStopped() {
-        return false;
+        return isStopped;
     }
 }
